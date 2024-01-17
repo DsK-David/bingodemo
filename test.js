ea9bfd9606f04949eb1fc670c3ea56f6be8a30b5
DsK-David <fbi.gov2015@gmail.com>
2024-01-17 09:49:02 -0100

    nova versão :tada:

--- test.js
+++ test.js
@@ -1,22 +1,23 @@
-import { green } from 'chalk';
-import { question } from 'readline';
-import { random } from './index';
+import chalk  from 'chalk';
+import  { question }  from 'readline-sync';
+import { random } from './index.js';
 function bingo(){
     const NumerosSorteados = new Set()
-    const totalNumeros = 90
+    const totalNumeros = 3
     let rodada = 1
 
     while(NumerosSorteados.size<totalNumeros){
-        const NumeroSorteado = random()
+        const NumeroSorteado = 1234
         if(!NumerosSorteados.has(NumeroSorteado)){
             NumerosSorteados.add(NumeroSorteado);
             console.log(`rodada ${rodada++}:${NumeroSorteado}`)
         }
         if(NumeroSorteado === numeroEscolhido){
             console.log(chalk.green(`Parabens! seu numero ${numeroEscolhido} foi sorteado`))
+           
         }
     }
-    console.log('bingo completo')
+    console.log('bingo completo seu numero não foi sorteado')
 }
-const numeroEscolhido = parseInt(question('Escolha um numero de 1000 a 9999'),10)
+const numeroEscolhido = parseInt(question('Escolha um numero de 1000 a 9999:'),10)
 bingo()
